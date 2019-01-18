fcabc1382a8cd49a9a3fa89792f2462f8f44a27d
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-01-18 17:30:56 +0700

    Add files via upload

---
+++ MidTermTest03.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    int[][] n = {{3},
+                {2,5},
+                {3,5,2},
+                {2,1,1,3}};
+    printStep(n,1);
+    printStep(n,2);
+    printStep(n,3);
+  }
+  public static void printStep(int[][] n ,int step){
+    int count = 0 ;
+    System.out.print(n[0][0] + " ");
+    for(int i=0;i<n.length;i++)
+      for(int j=0;j<n[i].length;j++){
+        if(count == step){
+          System.out.print(n[i][j] + " ");
+          count = 0;
+        }
+        count++;
+      }
+      System.out.println(" ");
+  }
+}
