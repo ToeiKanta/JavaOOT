ce01a1b6811583f25fd19728ca44b3459b617751
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-01-30 21:20:31 +0700

    Update Teacher.java

--- Lab 07/lab7-1_B6000530 [sec 2]/src/oot/lab7/Teacher.java
+++ Lab 07/lab7-1_B6000530 [sec 2]/src/oot/lab7/Teacher.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package oot.lab7;
 
-/**
- *
- * @author ToeiK
- */
 public class Teacher extends Person{
   double salary;
   Teacher(String n,String g, double s){
